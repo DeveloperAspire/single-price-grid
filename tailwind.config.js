174a5c8f75154471818b4c00134eb453c11f67b6
Franklin <franklinokolie86@gmail.com>
2021-01-08 16:50:03 +0100

    Made the single price grid
    Signed-off-by: Franklin <franklinokolie86@gmail.com>

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      cyan: "hsl(179, 62%, 43%)",
+      yellow: "hsl(71, 73%, 54%)",
+      gray: "hsl(204, 43%, 93%)",
+      grayblue: "hsl(218, 22%, 67%)",
+      purple: "hsl(228, 45%, 44%)",
+      white: "#ffff"
+    },
+    fontFamily:{
+      "karla":['Karla','sans-serif']
+    },
+    fontWeight:{
+      "semi-bold": "400",
+      "extra-bold": "700"
+    },
+    extend: {
+    
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
